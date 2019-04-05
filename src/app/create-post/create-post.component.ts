946cf433e30a18b218be74bd9d6e7b49b88912ab
Dave Sarbora <dsarbora@gmail.com>
2019-04-05 15:55:34 -0700

    create post model

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css']
 })
-export class CreatePostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class CreatePostComponent{
+  createPost(postText: string){
+    let post: Post = new Post(postText);
   }
-
 }
