763d276340b20cc4e79e97cda265b58691da5d0c
Dave Sarbora <dsarbora@gmail.com>
2019-04-12 11:51:01 -0700

    incorporate mock profile

---
+++ src/app/mock-profiles.ts
@@ -0,0 +1,6 @@
+import { Profile } from './models/profile.model';
+import { Post } from './models/post.model';
+
+export const PROFILES: Profile[] =[
+    new Profile("John", "Wayne", "jwayne@fakebook.com", "pardner")
+]
