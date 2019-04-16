f4783fbaa056e3f3605f4e587846f54acfd0e92b
Dave Sarbora <dsarbora@gmail.com>
2019-04-16 00:57:52 -0700

    add post service

--- src/app/mock-profiles.ts
+++
@@ -1,6 +0,0 @@
-import { Profile } from './models/profile.model';
-import { Post } from './models/post.model';
-
-export const PROFILES: Profile[] =[
-    new Profile("John", "Wayne", "jwayne@fakebook.com", "pardner")
-]
