193de27033891171e7ead8fa06dfecb5b323bc26
Dave Sarbora <dsarbora@gmail.com>
2019-04-16 01:04:06 -0700

    add post methods to post service

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Post } from './models/post.model';
 
 @Injectable()
 export class PostService {
+  posts: FirebaseListObservable<any[]>
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.posts = database.list("posts");
+   }
+
+   getPosts(){
+     return this.posts;
+   }
+
+   getPostById(){
+     
+   }
+
+   createPost(newPost: Post){
+     this.posts.push(newPost);
+   }
 
 }
